b92fc27a70ed5e54c955ee9cbd3cead8d70a02af
alexandr <KalX_si54@student.itstep.org>
2024-01-28 18:54:45 +0200

    dark-mode

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,4 +1,6 @@
 const darkbtn = document.getElementById("darkBtn");
+const body = document.getElementById("body");
+
 let darkMode = false;
 function darkClick()
 {
@@ -6,11 +8,13 @@ function darkClick()
     {
         darkbtn.style.marginLeft = "1.5vw";
         darkMode = true;
+        body.classList.add("dark-body");
     }
     else
     {
         darkbtn.style.marginLeft = "0.2vw";
         darkMode = false;
+        body.classList.remove("dark-body");
     }
     console.log("hello world");
 }
